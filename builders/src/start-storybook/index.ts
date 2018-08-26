72ffe4cd346692ef238a0c02e4503d4bb70d5c87
Jonathan Gelin <gelin.jonathan@gmail.com>
2018-08-26 09:49:00 +0200

    make it start

--- builders/src/start-storybook/index.ts
+++ builders/src/start-storybook/index.ts
@@ -1,20 +1,34 @@
 import { Builder, BuilderConfiguration, BuilderContext, BuildEvent } from '@angular-devkit/architect';
-import { from, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { StartStorybookSchema } from './schema';
 import { buildDev } from '@storybook/core/server';
+import program from 'commander';
 
 import storyBookOptions from '@storybook/angular/dist/server/options';
+
+import jsonSchema from './schema.json';
 
 export default class StartStorybookBuilder implements Builder<StartStorybookSchema> {
   constructor(private context: BuilderContext) {}
 
   run(builderConfig: BuilderConfiguration<Partial<StartStorybookSchema>>): Observable<BuildEvent> {
-    console.log('OK');
-
-    return of(null).pipe(
+    return of(builderConfig).pipe(
+      initArgumentsFromOptions(),
       tap(_ => buildDev(storyBookOptions)),
       map(() => ({ success: true }))
     );
   }
+}
+
+export function initArgumentsFromOptions() {
+  return tap(({ options, root }) => {
+    // init storybook arguments = command argument OR builder option
+    Object.keys(jsonSchema.properties).forEach(key => {
+      program[key] = program[key] || Array.isArray(options[key]) ? options[key].join(',') : options[key];
+    });
+
+    //set default root config dir if not specified
+    program.configDir = program.configDir || root;
+  });
 }
